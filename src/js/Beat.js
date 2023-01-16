284e03e6eb0c48fe05538aef6b1b9956e831b8ad
Yosif Georgiev <iodesign@live.com>
2023-01-16 14:59:48 +0200

    FIX: event

--- src/js/Beat.js
+++ src/js/Beat.js
@@ -1,6 +1,6 @@
-// import EventEmitter from 'eventemitter3';
+import EventEmitter from 'eventemitter3';
 
-import * as EventEmitter from 'eventemitter3'
+
 
 export default class Beat extends EventEmitter{
   static get events() {
